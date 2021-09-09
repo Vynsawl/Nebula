558a1af7c609e9e14dd5ee50d0e3ded5ffea52d6
Maxime Thureau <thureau13@gmail.com>
2021-09-09 10:24:12 +0200

    add unity project

---
+++ Assets/AirPressure.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirPressure : MonoBehaviour {
+
+    private Material nebulaMat;
+    private ReadOneArduinoValueExample arduino;
+    public float airPressure;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nebulaMat = gameObject.GetComponent<MeshRenderer>().sharedMaterial;
+        arduino = FindObjectOfType<ReadOneArduinoValueExample>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        airPressure = arduino.value;
+        nebulaMat.SetFloat("_NoiseScale", arduino.value);
+
+
+    }
+}
